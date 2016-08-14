44ed82fddee26faa306e865d349d8cb4923793d4
Yunspace <yun@yunspace.com>
2016-08-14 23:34:40 +1000

    added bootstrap ps1 script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-dotfiles-windows
+# dotfiles-windows
+
+My Windows setup using the awesome [lukesampson/scoop](https://github.com/lukesampson/scoop/). An single powershell script that installs everything via mostly github. No need to fuss around with OneGet or Chocolatey 
---
+++ bootstrap.ps1
@@ -0,0 +1,41 @@
+#requires -v 3
+
+if (Get-Command scoop -errorAction SilentlyContinue)
+{
+	write-host "scoop already installed"
+} else {
+	iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+	set-executionpolicy unrestricted -s cu
+}
+
+# utils
+scoop install 7zip curl sudo git openssh coreutils grep sed less which
+[environment]::setenvironmentvariable('GIT_SSH', (resolve-path (scoop which ssh)), 'USER')
+
+# programming languages
+scoop install python ruby go nodejs oraclejdk
+
+# docker
+scoop install docker docker-compose rancher-compose
+
+# dev tools
+scoop install vagrant apex sublime-text winmerge hugo
+
+# misc
+scoop install vlc
+
+# vim
+scoop install vim
+'
+set ff=unix
+
+set cindent
+set tabstop=4
+set shiftwidth=4
+set expandtab
+set backupdir=$TEMP
+' | out-file ~/.vimrc -enc oem -append
+
+# console theme
+scoop install concfg pshazz
+concfg import solarized small
