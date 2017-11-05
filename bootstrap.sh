cdc5cc281828f18b7d94f964533490d334a8f896
Yun Zhi Lin <yun@yunspace.com>
2017-11-06 00:25:21 +1100

    added go and docker-compose installations

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,4 +9,12 @@ sudo usermod -aG docker $(whoami)
 
 echo "export DOCKER_HOST='tcp://0.0.0.0:2375'" >> ~/.bashrc
 
+# install docker-compose
+sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/latest/docker-compose-Linux-x86_64"
+
+# install go
+wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.9.linux-amd64.tar.gz
+rm go1.9.linux-amd64.tar.gz
+
 source ~/.bashrc
