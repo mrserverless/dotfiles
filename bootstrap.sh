fde8a6175665c952305b3e5f544d31a59c0bbc0c
Yun Zhi Lin <yun@yunspace.com>
2017-11-06 00:29:29 +1100

    Added pip and awscli

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
+sudo apt-get upgrade
 
-# install docker
+# docker
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
@@ -9,12 +10,19 @@ sudo usermod -aG docker $(whoami)
 
 echo "export DOCKER_HOST='tcp://0.0.0.0:2375'" >> ~/.bashrc
 
-# install docker-compose
+# docker-compose
 sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/latest/docker-compose-Linux-x86_64"
 
-# install go
+# go
 wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.9.linux-amd64.tar.gz
 rm go1.9.linux-amd64.tar.gz
+
+# install pip
+sudo apt-get install python-pip
+pip install --upgrade pip
+
+# aws cli
+pip install awscli
 
 source ~/.bashrc
