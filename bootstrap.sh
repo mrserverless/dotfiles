0c31e29e84f68cf9a0adc75969cdb0d5b8e9dcce
Yun Zhi Lin <yun@yunspace.com>
2018-01-21 21:42:57 +1100

    added -y to installs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,41 +1,42 @@
-sudo apt-get upgrade
-
-# utility
-sudo apt-get install node-gyp
+sudo apt-get update
+sudo apt-get -y upgrade
 
 # docker
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get -y install docker-ce
 sudo usermod -aG docker $(whoami)
 
 echo "export DOCKER_HOST='tcp://0.0.0.0:2375'" >> ~/.bashrc
 
 # docker-compose
-sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/latest/docker-compose-Linux-x86_64"
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # go
 wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.9.linux-amd64.tar.gz
 rm go1.9.linux-amd64.tar.gz
 
-# nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+# utility
+sudo apt-get -y install node-gyp python3
 
 # aws cli
 pip3 install awscli
 
 # java
-sudo apt-get -o Dpkg::Options::="--force-overwrite" install openjdk-9-jdk
+sudo apt-get -y -o Dpkg::Options::="--force-overwrite" install openjdk-9-jdk
 
 # dotnet
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
-
 sudo apt-get update
 sudo apt-get install dotnet-sdk-2.0.3
+
+# nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
 source ~/.bashrc
