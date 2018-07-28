2d29ae6998adfac88ecdec4dbcfe56b5ea177c87
Yunspace <yun@yunspace.com>
2018-07-28 20:23:48 +1000

    add unison

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ sudo apt -y upgrade
 sudo apt -y install ca-certificates build-essential libssl-dev zip make dos2unix curl jq
 
 # programming languages
-sudo apt -y -o Dpkg::Options::="--force-overwrite" install python python-pip python3 python3-pip openjdk-9-jdk ruby
+sudo apt -y -o Dpkg::Options::="--force-overwrite" install python python-pip python3 python3-pip openjdk-9-jdk ruby ruby-dev
 sudo apt -y -o install node-gyp
 
 # go
@@ -42,6 +42,9 @@ echo "export DOCKER_HOST='tcp://0.0.0.0:2375'" >> ~/.bashrc
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# docker-sync
+sudo gem install docker-sync
+
 # protoc
 curl -L https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip 
 sudo unzip /usr/local/
@@ -49,7 +52,29 @@ sudo chmod a+x /usr/local/bin/protoc
 source ~/.bashrc
 
 # watchexec
-sudo curl h -L https://github.com/mattgreen/watchexec/releases/download/1.8.6/watchexec-1.8.6-x86_64-unknown-linux-gnu.tar.gz | sudo tar -xz -C /usr/local/bin --strip-components=1
+sudo curl -L https://github.com/mattgreen/watchexec/releases/download/1.8.6/watchexec-1.8.6-x86_64-unknown-linux-gnu.tar.gz | sudo tar -xz -C /usr/local/bin --strip-components=1
+
+# ocaml
+wget http://caml.inria.fr/pub/distrib/ocaml-4.06/ocaml-4.06.0.tar.gz
+tar xvf ocaml-4.06.0.tar.gz
+cd ocaml-4.06.0
+./configure
+make world
+make opt
+umask 022
+sudo make install
+sudo make clean
+
+# unison
+wget https://github.com/bcpierce00/unison/archive/v2.51.2.tar.gz
+tar xvf v2.51.2.tar.gz
+cd unison-2.51.2
+make UISTYLE=text
+sudo cp src/unison /usr/local/bin/unison
+sudo cp src/unison-fsmonitor /usr/local/bin/unison-fsmonitor
+
+# timezone
+sudo dpkg-reconfigure tzdata
 
 # upgrade Ubuntu OS
 # sudo -S apt-mark hold procps strace sudo
