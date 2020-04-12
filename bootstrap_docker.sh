70d05f02fa9bec78271385840d1351de6f556ab5
Yun Zhi Lin <yun.zhi.lin@contino.io>
2020-04-13 00:24:50 +1000

    add docker-compose

--- bootstrap_docker.sh
+++ bootstrap_docker.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
 # docker
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y install docker.io
 sudo usermod -aG docker $USER
+
+# docker-compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
