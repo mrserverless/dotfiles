e67ea4b2f59bb8f73d23991ddcd0bcbf7c02bc3c
Yun Zhi Lin <yun@yunspace.com>
2020-08-23 14:02:50 +1000

    use n instead of nvm

--- boostrap_common.sh
+++ boostrap_common.sh
@@ -22,7 +22,7 @@ sudo apt-get update
 sudo apt-get install aspnetcore-runtime-2.1
 
 # nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+npm install -g n
 
 # dev tools
 pip3 install awscli
