7b2ca9bbe641727b7dc98c3a78b743f77231c816
Yun Zhi Lin <yun.zhi.lin@contino.io>
2020-09-13 12:32:27 +1000

    Update popos.sh

--- Linux/popos.sh
+++ Linux/popos.sh
@@ -1,10 +1,13 @@
 
+# reset theme 
+gsettings set org.gnome.desktop.interface icon-theme "Pop"
+gsettings set org.gnome.desktop.interface cursor-theme "Pop"
 
 # dev
 sudo apt -y install code go awscli dbeaver-ce hugo
 
 # tools
-sudo apt -y install chromium slack spotify-client virt-manager
+sudo apt -y install chromium slack-desktop spotify-client virt-manager github-desktop
 
 # games
 sudo apt -y install steam lutrix wine-stable
