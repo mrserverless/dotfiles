7b6682730c28628766d46941b177ebab617b3a50
Yun Zhi Lin <yun@yunspace.com>
2021-07-09 00:42:23 +1000

    updated java and docker-compose versions

--- Linux/extras/docker.sh
+++ Linux/extras/docker.sh
@@ -8,5 +8,7 @@ sudo usermod -aG docker $USER
 sudo apt install docker-compose
 
 # docker-compose manual
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# podman
